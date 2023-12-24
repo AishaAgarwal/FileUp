ad7b1e3be33244511a5d912cba3c5dc2c81a7681
AishaAgarwal <aishaagarwal19567@gmail.com>
2023-12-24 18:12:57 +0530

    format the code

--- app/page.tsx
+++ app/page.tsx
@@ -57,17 +57,17 @@ const Home: FC<HomeProps> = () => {
             <div className="flex items-center">
               <Image src="search.svg" height={32} width={32} alt="search" />
               <span className="ml-2 text-white">Search</span>
-             
             </div>
-           
           </div>
-         
+
           <div
             className="p-8 opacity-40 backdrop-blur-[90px] backdrop-filter w-[1000px] h-[600px] relative rounded-r-[30px] text-white"
             style={{ backgroundColor: "rgba(255, 255, 255, 0.2)" }}
           >
             {/* Your content inside the box */}
-            <div className="mt-20 left 20 text-white text-2xl font-bold mb-2">&#62; &nbsp;My Files</div>
+            <div className="mt-20 left 20 text-white text-2xl font-bold mb-2">
+              &#62; &nbsp;My Files
+            </div>
             {/* Four boxes placed horizontally in the middle with top margin */}
             <div className="flex justify-between text-white">
               {/* Box 1 */}
@@ -77,15 +77,24 @@ const Home: FC<HomeProps> = () => {
               >
                 {/* Content inside Box 1 */}
                 <div className="flex items-center">
-              <Image src="folder.svg" height={32} width={32} alt="search" className="mr-auto" />
-              <Image src="dots.svg" height={32} width={32} alt="search" className="ml-auto" />
-              
-             
-            </div>
-            <span className="ml-2 text-white">Touch</span>
-            <br />
-            <span className="ml-2 text-white">2358 items</span>
-
+                  <Image
+                    src="folder.svg"
+                    height={32}
+                    width={32}
+                    alt="search"
+                    className="mr-auto"
+                  />
+                  <Image
+                    src="dots.svg"
+                    height={32}
+                    width={32}
+                    alt="search"
+                    className="ml-auto"
+                  />
+                </div>
+                <span className="ml-2 text-white">Touch</span>
+                <br />
+                <span className="ml-2 text-white">2358 items</span>
               </div>
 
               {/* Box 2 */}
@@ -95,15 +104,24 @@ const Home: FC<HomeProps> = () => {
               >
                 {/* Content inside Box 2 */}
                 <div className="flex items-center">
-              <Image src="folder.svg" height={32} width={32} alt="search" className="mr-auto" />
-              <Image src="dots.svg" height={32} width={32} alt="search" className="ml-auto" />
-              
-             
-            </div>
-            <span className="ml-2 text-white">Documents</span>
-            <br />
-            <span className="ml-2 text-white">2358 items</span>
-
+                  <Image
+                    src="folder.svg"
+                    height={32}
+                    width={32}
+                    alt="search"
+                    className="mr-auto"
+                  />
+                  <Image
+                    src="dots.svg"
+                    height={32}
+                    width={32}
+                    alt="search"
+                    className="ml-auto"
+                  />
+                </div>
+                <span className="ml-2 text-white">Documents</span>
+                <br />
+                <span className="ml-2 text-white">2358 items</span>
               </div>
 
               {/* Box 3 */}
@@ -113,15 +131,24 @@ const Home: FC<HomeProps> = () => {
               >
                 {/* Content inside Box 3 */}
                 <div className="flex items-center">
-              <Image src="dropbox.svg" height={32} width={32} alt="search" className="mr-auto" />
-              <Image src="dots.svg" height={32} width={32} alt="search" className="ml-auto" />
-              
-             
-            </div>
-            <span className="ml-2 text-white">Dropbox</span>
-            <br />
-            <span className="ml-2 text-white">External Storage</span>
-
+                  <Image
+                    src="dropbox.svg"
+                    height={32}
+                    width={32}
+                    alt="search"
+                    className="mr-auto"
+                  />
+                  <Image
+                    src="dots.svg"
+                    height={32}
+                    width={32}
+                    alt="search"
+                    className="ml-auto"
+                  />
+                </div>
+                <span className="ml-2 text-white">Dropbox</span>
+                <br />
+                <span className="ml-2 text-white">External Storage</span>
               </div>
 
               {/* Box 4 */}
@@ -131,15 +158,24 @@ const Home: FC<HomeProps> = () => {
               >
                 {/* Content inside Box 4 */}
                 <div className="flex items-center">
-              <Image src="drive.svg" height={32} width={32} alt="search" className="mr-auto" />
-              <Image src="dots.svg" height={32} width={32} alt="search" className="ml-auto" />
-              
-             
-            </div>
-            <span className="ml-2 text-white">Google Drive</span>
-            <br />
-            <span className="ml-2 text-white">External Storage</span>
-
+                  <Image
+                    src="drive.svg"
+                    height={32}
+                    width={32}
+                    alt="search"
+                    className="mr-auto"
+                  />
+                  <Image
+                    src="dots.svg"
+                    height={32}
+                    width={32}
+                    alt="search"
+                    className="ml-auto"
+                  />
+                </div>
+                <span className="ml-2 text-white">Google Drive</span>
+                <br />
+                <span className="ml-2 text-white">External Storage</span>
               </div>
             </div>
           </div>
@@ -154,63 +190,65 @@ const Home: FC<HomeProps> = () => {
               Recent Files
             </div>
             <div className="flex absolute left-10 top-10 inset-x-0 mx-auto mt-8 text-white">
-  <div className="w-1/4 pr-4">Files</div>
-  <div className="w-1/4 pr-4">Date</div>
-  <div className="w-1/4 pr-4">Size</div>
-  <div className="w-1/4 pr-4">Kind</div>
-</div>
+              <div className="w-1/4 pr-4">Files</div>
+              <div className="w-1/4 pr-4">Date</div>
+              <div className="w-1/4 pr-4">Size</div>
+              <div className="w-1/4 pr-4">Kind</div>
+            </div>
 
-<div className="flex absolute left-7 top-20 inset-x-0 mx-auto mt-8 text-white">
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-  <Image src="adobe.svg" height={20} width={20} alt="search" />
-    <span className="ml-2 text-white">Instructor</span>
-  </div>
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-    <span className="ml-2 text-white">27 February 2023, 7:45 p.m.</span>
-  </div>
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-    <span className="ml-2 text-white">46 MB</span>
-  </div>
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-    <span className="ml-2 text-white">PDF</span>
-  </div>
-</div>
+            <div className="flex absolute left-7 top-20 inset-x-0 mx-auto mt-8 text-white">
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <Image src="adobe.svg" height={20} width={20} alt="search" />
+                <span className="ml-2 text-white">Instructor</span>
+              </div>
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <span className="ml-2 text-white">
+                  27 February 2023, 7:45 p.m.
+                </span>
+              </div>
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <span className="ml-2 text-white">46 MB</span>
+              </div>
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <span className="ml-2 text-white">PDF</span>
+              </div>
+            </div>
 
-<div className="flex absolute left-7 top-40 inset-x-0 mx-auto mt-8 text-white">
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-  <Image src="cash.svg" height={20} width={20} alt="search" />
-    <span className="ml-2 text-white top-30">Cashflow Records</span>
-  </div>
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-    <span className="ml-2 text-white">28 February 2023</span>
-  </div>
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-    <span className="ml-2 text-white">30 MB</span>
-  </div>
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-    <span className="ml-2 text-white">PPT</span>
-  </div>
-</div>
+            <div className="flex absolute left-7 top-40 inset-x-0 mx-auto mt-8 text-white">
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <Image src="cash.svg" height={20} width={20} alt="search" />
+                <span className="ml-2 text-white top-30">Cashflow Records</span>
+              </div>
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <span className="ml-2 text-white">28 February 2023</span>
+              </div>
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <span className="ml-2 text-white">30 MB</span>
+              </div>
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <span className="ml-2 text-white">PPT</span>
+              </div>
+            </div>
 
-{/* Add similar blocks for "Doc" and "Article" rows */}
+            {/* Add similar blocks for "Doc" and "Article" rows */}
 
-<div className="flex absolute left-7 top-60 inset-x-0 mx-auto mt-8 text-white">
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-  <Image src="word.svg" height={20} width={20} alt="search" />
-    <span className="ml-2 text-white top-30">Proposal Doc</span>
-  </div>
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-    <span className="ml-2 text-white">28 February 2023, 7.50p.m.</span>
-  </div>
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-    <span className="ml-2 text-white">30 MB</span>
-  </div>
-  <div className="w-1/4 pr-4 border-b border-white flex items-center">
-    <span className="ml-2 text-white">PPT</span>
-  </div>
-</div>
-
-            
+            <div className="flex absolute left-7 top-60 inset-x-0 mx-auto mt-8 text-white">
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <Image src="word.svg" height={20} width={20} alt="search" />
+                <span className="ml-2 text-white top-30">Proposal Doc</span>
+              </div>
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <span className="ml-2 text-white">
+                  28 February 2023, 7.50p.m.
+                </span>
+              </div>
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <span className="ml-2 text-white">30 MB</span>
+              </div>
+              <div className="w-1/4 pr-4 border-b border-white flex items-center">
+                <span className="ml-2 text-white">PPT</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -226,11 +264,16 @@ const Home: FC<HomeProps> = () => {
           style={{ backgroundColor: "rgba(255, 255, 255, 0.3)" }}
         >
           {/* Content inside the box inside the box below the three boxes */}
-          <Image src="avatar2.svg" height={32} width={32} alt="search" className="mr-auto" />
+          <Image
+            src="avatar2.svg"
+            height={32}
+            width={32}
+            alt="search"
+            className="mr-auto"
+          />
           <span className="text-white text-sm">Hello &nbsp;</span>
-            <br />
-            <span className="text-white text-sm">Aisha</span>
-
+          <br />
+          <span className="text-white text-sm">Aisha</span>
         </div>
 
         <div className="absolute inset-0 flex items-center justify-center">
@@ -240,9 +283,15 @@ const Home: FC<HomeProps> = () => {
           >
             {/* Content inside the centered box */}
             <div className="flex items-center">
-  <Image src="spotify.svg" height={32} width={32} alt="search" className="mr-2" />
-  <span className="text-white text-sm">See yoe </span>
-</div>
+              <Image
+                src="spotify.svg"
+                height={32}
+                width={32}
+                alt="search"
+                className="mr-2"
+              />
+              <span className="text-white text-sm">See yoe </span>
+            </div>
           </div>
         </div>
         <div className="absolute right-0 top-0 flex">
@@ -252,9 +301,14 @@ const Home: FC<HomeProps> = () => {
             style={{ backgroundColor: "rgba(255, 255, 255, 0.3)" }}
           >
             {/* Content inside Box 1 on the right */}
-            <Image src="work.svg" height={32} width={32} alt="search" className="mr-auto" />
-          <span className="text-white text-sm">Work</span>
-           
+            <Image
+              src="work.svg"
+              height={32}
+              width={32}
+              alt="search"
+              className="mr-auto"
+            />
+            <span className="text-white text-sm">Work</span>
           </div>
 
           {/* Box 2 on the right */}
@@ -264,8 +318,6 @@ const Home: FC<HomeProps> = () => {
           >
             {/* Content inside Box 2 on the right */}
             <Image src="avatar.svg" height={60} width={60} alt="search" />
-            
-
           </div>
         </div>
 
